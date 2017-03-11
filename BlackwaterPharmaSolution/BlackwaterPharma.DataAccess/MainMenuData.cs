a7f75fd1aeb5aab38a1ee272e48b065d46e3fd41
robertsilver <robert.silver@outlook.com>
2017-03-11 17:13:31 +0000

    Added the main menu data

---
+++ BlackwaterPharmaSolution/BlackwaterPharma.DataAccess/MainMenuData.cs
@@ -0,0 +1,20 @@
+﻿
+namespace BlackwaterPharma.DataAccess
+{
+    public class MainMenuData
+    {
+        private int B2BCustomer { get; set; }
+        private string Description { get; set; }
+        private int Display { get; set; }
+        private int Distributor { get; set; }
+        private int Everyone { get; set; }
+        private int MenuId { get; set; }
+        private int ParentId { get; set; }
+        private bool? SpecialAccount { get; set; }
+        private bool? SuperUser { get; set; }
+        private string Text;
+        private string URL { get; set; }
+        private int Vendor { get; set; }
+        private string WhoCanView { get; set; }
+    }
+}
